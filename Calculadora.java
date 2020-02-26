78446eab8f11e7fcccb81747ff232ecf7d8edd8b
Cesar Dias <cesarvdias@hotmail.com>
2020-02-26 15:34:07 -0300

    Primeira Versao

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+    public static void main (String [] args) {
+        Scanner entrada = new Scanner (System.in);
+
+        int num1 = 0;
+        int num2 = 0;
+        int soma = 0;
+
+        System.out.print("Digite o primeiro numero: ");
+        num1 = entrada.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        num2 = entrada.nextInt();
+
+        soma = num1 = num2;
+
+        System.out.print("A soma é %d\n", soma);
+    }
+}
