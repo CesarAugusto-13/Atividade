3e9b25c0f6244d60b6713c4944949d4a9c7a3bc8
Cesar Dias <cesarvdias@hotmail.com>
2020-02-26 16:15:31 -0300

    Multiplicação e Divisão

--- Calculadora.java
+++ Calculadora.java
@@ -5,6 +5,9 @@ public class Calculadora {
         int num1 = 0;
         int num2 = 0;
         int soma = 0;
+        int sub = 0;
+        int multi = 0;
+        int div = 0;
 
         System.out.print("Digite o primeiro numero: ");
         num1 = entrada.nextInt();
@@ -14,8 +17,12 @@ public class Calculadora {
 
         soma = num1 = num2;
         sub = num1 - num2;
+        multi = num1 * num2; 
+        div = num1 / num2;
 
         System.out.print("A soma é %d\n", soma);
         System.out.print("A subtração é %d\n", sub);
+        System.out.print("A multiplicação é %d\n", multi);
+        System.out.print("A divisão é %d\n", div);
     }
 }
